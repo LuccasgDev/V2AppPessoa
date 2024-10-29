6c8144d4a9f38a8147a550e83d403dc755114e87
LuccasgDev <163527503+LuccasgDev@users.noreply.github.com>
2024-10-29 18:04:18 -0300

    Ajutes

--- App.tsx
+++ App.tsx
@@ -20,7 +20,7 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <StackRoutes /> {/* Mantém as rotas */}
+      <StackRoutes />
       <StatusBar style="auto" />
     </NavigationContainer>
   );
--- src/constants/links.tsx
+++ src/constants/links.tsx
@@ -1,2 +1,2 @@
 // src/constants/links.ts
-export const API_BASE_URL = 'https://localhost:7281/api'; // Ou ajuste conforme necessário
+export const API_BASE_URL = 'http://192.168.105.72:5153/api';
